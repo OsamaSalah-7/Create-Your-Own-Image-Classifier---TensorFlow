2f870e96f809d15e35da646a58d4d8838492c423
Osama Salah <159712268+OsamaSalah-7@users.noreply.github.com>
2024-08-22 04:00:58 +0300

    Update predict.py

--- Flower-Image-Classifier-main/Flower-Image-Classifier-main/predict.py
+++ Flower-Image-Classifier-main/Flower-Image-Classifier-main/predict.py
@@ -1,54 +1,50 @@
 import os
 import json
-import time
-
+import argparse
 import numpy as np
-import argparse as arg
 from PIL import Image
-
 import tensorflow as tf
 import tensorflow_hub as hub
 
+def load_trained_model(model_directory):
+    model = tf.keras.models.load_model(model_directory, custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
+    return model
 
+def preprocess_image(image_array):
+    target_size = 224
+    image_array = tf.image.resize(image_array, (target_size, target_size))
+    image_array = tf.cast(image_array, tf.float32) / 255.0
+    return image_array.numpy()
 
-def load_model(path):
-    model_path = './' + path
-    load_model = tf.keras.models.load_model(path ,custom_objects={'KerasLayer':hub.KerasLayer}, compile=False)
-    return load_model
-                                                 
-def process_image(image):
-    image_size = 224
-    image = tf.cast(image, tf.float32)
-    image = tf.image.resize(image, (image_size, image_size))
-    image /= 255
-    image = image.numpy()
-    return image    
-    
-def predict(image_path, model, top_k=5):
-    im = Image.open(image_path)
-    test_image = np.asarray(im)
-    processed_test_image = process_image(test_image)
-    processed_test_image = np.expand_dims(processed_test_image, axis=0)
-    preds = model.predict(processed_test_image)
-    probs = - np.partition(-preds[0], top_k)[:top_k]
-    classes = np.argpartition(-preds[0], top_k)[:top_k]
-    return probs, classes
+def make_prediction(image_file_path, trained_model, top_k_predictions=5):
+    image = Image.open(image_file_path)
+    image_as_array = np.asarray(image)
+    processed_image = preprocess_image(image_as_array)
+    processed_image = np.expand_dims(processed_image, axis=0)
 
-parser = arg.ArgumentParser()
+    prediction_values = trained_model.predict(processed_image)[0]
+    top_class_indices = np.argsort(-prediction_values)[:top_k_predictions]
+    top_probabilities = prediction_values[top_class_indices]
+    return top_probabilities, top_class_indices
 
-parser.add_argument('img_path', type=str)
-parser.add_argument('model_path', type=str)
-parser.add_argument('--top_k', type=int, default=5)
-parser.add_argument('--category_names', type=str, required=True)
+def main():
+    parser = argparse.ArgumentParser(description='Image Classification Prediction Tool')
+    parser.add_argument('image_file', type=str, help='File path of the image to classify')
+    parser.add_argument('model_file', type=str, help='File path of the pre-trained model')
+    parser.add_argument('--top_k', type=int, default=5, help='Return the top K predicted classes')
+    parser.add_argument('--label_map', type=str, required=True, help='File path to a JSON file mapping labels to human-readable names')
 
-args = parser.parse_args()
-model = load_model(args.model_path)
+    arguments = parser.parse_args()
 
-prob, classes = predict(image_path=args.img_path, model=model, top_k=args.top_k)
+    model = load_trained_model(arguments.model_file)
+    probabilities, class_indices = make_prediction(arguments.image_file, model, arguments.top_k)
 
-with open(args.category_names, 'r') as f:
-    class_names = json.load(f)
-    classes = {int(i): class_names[str(i)] for i in classes}
-    
-print('Predictions:', classes)
-print('Probability:', prob)
+    with open(arguments.label_map, 'r') as json_file:
+        labels_map = json.load(json_file)
+        readable_class_labels = [labels_map[str(index)] for index in class_indices]
+
+    print('Predicted Classes:', readable_class_labels)
+    print('Associated Probabilities:', probabilities)
+
+if __name__ == '__main__':
+    main()
